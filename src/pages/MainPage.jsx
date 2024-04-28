c3ea823153c259d6b61bad39f532d93472eec576
Vadym <sirenkovadim18@gmail.com>
2024-04-28 12:37:37 +0300

    [CHANGE] add adaptive mobile

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -6,18 +6,21 @@ const MainPage = () => {
     <div className="main">
         <h1 className="main__title">How much have you lost/earned on Bitcoin?</h1>
         <div className="main__content">
+            <div className='main__content__result'></div>
             <form className="main__content__form">
-                <Slider
-                    min={100}
-                    max={1000}
-                    label="Investment"
-                    unit="$"
-                />
-                <Slider
-                    min={2012}
-                    max={2024}
-                    label="Investment date"
-                />
+                <div className='main__content__form__sliders'>
+                    <Slider
+                        min={100}
+                        max={1000}
+                        label="Investment"
+                        unit="$"
+                    />
+                    <Slider
+                        min={2012}
+                        max={2024}
+                        label="Investment date"
+                    />
+                </div>
 
                 <button
                     type="submit"
